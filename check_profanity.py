bb386c0801055d726305c1cfa9595ac0ccc3949b
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-01-08 13:43:05 -0600

    Profanity checker

---
+++ check_profanity.py
@@ -0,0 +1,20 @@
+import urllib
+
+def read_text():
+    quotes = open("movie_quotes.txt")
+    contents_of_file =  quotes.read()
+    print(contents_of_file)
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert!!!")
+    elif "false" in output:
+        print("This document has no curse words.")
+    else:
+        print("Couldn't scan document properly.")
+
+read_text()
