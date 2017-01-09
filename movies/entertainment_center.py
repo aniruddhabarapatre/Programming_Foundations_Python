4b50f00f7570b90db363bb4430b5f45d771e120b
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-01-08 22:24:25 -0600

    movie trailer website

---
+++ movies/entertainment_center.py
@@ -0,0 +1,16 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "A story of boy and his toys that come to life.",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                    "A marine on an alien planet",
+                    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+movies = [avatar, toy_story]
+
+fresh_tomatoes.open_movies_page(movies)
