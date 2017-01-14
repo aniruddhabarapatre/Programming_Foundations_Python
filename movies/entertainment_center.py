0c310094a9d3f9bef88dba2a6067f04ff55c01a2
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-01-14 11:21:05 -0600

    Added more movies

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -11,6 +11,22 @@ avatar = media.Movie("Avatar",
                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-movies = [avatar, toy_story]
+kubo = media.Movie("Kubo and the Two Strings",
+                    "A young boy named Kubo must locate a magical suit of armor worn by his late"
+                    " father in order to defeat a vengeful spirit from the past.",
+                    "https://upload.wikimedia.org/wikipedia/en/c/c4/Kubo_and_the_Two_Strings_poster.png",
+                    "https://www.youtube.com/watch?v=oPEz0EYihzE")
+
+jungle_book = media.Movie("The Jungle Book",
+                    "A story of a boy raised among wolves in jungle",
+                    "https://upload.wikimedia.org/wikipedia/en/a/a4/The_Jungle_Book_%282016%29.jpg",
+                    "https://www.youtube.com/watch?v=TmLRj9zm5SE")
+
+zootopia = media.Movie("Zootopia",
+                    "A bunny cop and a fox solves a mystery",
+                    "https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
+                    "https://www.youtube.com/watch?v=1ZOHlq6Qcz0")
+
+movies = [avatar, toy_story, kubo, jungle_book, zootopia]
 
 fresh_tomatoes.open_movies_page(movies)
