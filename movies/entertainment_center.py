d515fad34d14abb8db44caf5ba32d06cb337e1cf
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-01-21 16:56:28 -0600

    Added comments and confirmed to PEP8 guidelines

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -6,32 +6,41 @@ Contains list of movies to display
 Each movie consists of name, description, movie poster and trailer url
 '''
 
+# Create Movie objects with individual movie information
 toy_story = media.Movie("Toy Story",
                         "A story of boy and his toys that come to life.",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/"
+                        "Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
-                    "A marine on an alien planet",
-                    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-"
+                     "Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 kubo = media.Movie("Kubo and the Two Strings",
-                    "A young boy named Kubo must locate a magical suit of armor worn by his late"
-                    " father in order to defeat a vengeful spirit from the past.",
-                    "https://upload.wikimedia.org/wikipedia/en/c/c4/Kubo_and_the_Two_Strings_poster.png",
-                    "https://www.youtube.com/watch?v=oPEz0EYihzE")
+                   "A young boy named Kubo must locate a magical suit of "
+                   "armor worn by his late father in order to defeat a "
+                   "vengeful spirit from the past.",
+                   "https://upload.wikimedia.org/wikipedia/en/c/c4/"
+                   "Kubo_and_the_Two_Strings_poster.png",
+                   "https://www.youtube.com/watch?v=oPEz0EYihzE")
 
 jungle_book = media.Movie("The Jungle Book",
-                    "A story of a boy raised among wolves in jungle",
-                    "https://upload.wikimedia.org/wikipedia/en/a/a4/The_Jungle_Book_%282016%29.jpg",
-                    "https://www.youtube.com/watch?v=TmLRj9zm5SE")
+                          "A story of a boy raised among wolves in jungle",
+                          "https://upload.wikimedia.org/wikipedia/en/a/a4/"
+                          "The_Jungle_Book_%282016%29.jpg",
+                          "https://www.youtube.com/watch?v=TmLRj9zm5SE")
 
 zootopia = media.Movie("Zootopia",
-                    "A bunny cop and a fox solves a mystery",
-                    "https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
-                    "https://www.youtube.com/watch?v=1ZOHlq6Qcz0")
+                       "A bunny cop and a fox solves a mystery",
+                       "https://upload.wikimedia.org/wikipedia/en/e/ea/"
+                       "Zootopia.jpg",
+                       "https://www.youtube.com/watch?v=1ZOHlq6Qcz0")
 
+# Add each movie object to movies list
 movies = [avatar, toy_story, kubo, jungle_book, zootopia]
 
+# Open the movies list
 fresh_tomatoes.open_movies_page(movies)
